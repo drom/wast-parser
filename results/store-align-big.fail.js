bc1b9d6f4c7829554e635f0ef52156fbfa5ac43c
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2017-04-01 00:01:08 -0700

    bumped testsuite version

---
+++ results/store-align-big.fail.js
@@ -0,0 +1,48 @@
+{
+  kind: 'script',
+  body: [{
+    kind: 'module',
+    body: [
+      {
+        kind: 'memory',
+        int: {
+          kind: 'literal',
+          value: 0,
+          raw: [
+            [' '],
+            '0'
+          ]
+        },
+        int1: null,
+        segment: []
+      },
+      {
+        kind: 'func',
+        id: null,
+        expo: null,
+        imp: null,
+        type: null,
+        params: [],
+        result: null,
+        local: [],
+        body: [{
+          kind: 'store',
+          type: 'i64',
+          size: null,
+          offset: 0,
+          align: 16,
+          addr: {
+            kind: 'const',
+            type: 'i32',
+            init: '0'
+          },
+          data: {
+            kind: 'const',
+            type: 'i64',
+            init: '0'
+          }
+        }]
+      }
+    ]
+  }]
+}
